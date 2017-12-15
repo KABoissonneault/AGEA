dfba51cd1b527d8e1d909a9e7454d42a7331a1a1
Kévin Alexandre Boissonneault <kaboissonneault@gmail.com>
2017-12-14 20:23:04 -0500

    Moved view sdl

---
+++ include/view/sdl/sdl.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <SDL/SDL.h>
+#include <SDL/SDL_ttf.h>
+
+#include <memory>
+
+namespace hz::sdl {
+    struct texture_delete {
+        void operator()(SDL_Texture* p) {
+            if (p != nullptr) {
+                SDL_DestroyTexture(p);
+            }
+        }
+    };
+
+    struct surface_delete {
+        void operator()(SDL_Surface* p) {
+            SDL_FreeSurface(p);
+        }
+    };
+
+    struct window_delete {
+        void operator()(SDL_Window* p) {
+            if (p != nullptr) {
+                SDL_DestroyWindow(p);
+            }
+        }
+    };
+
+    struct renderer_delete {
+        void operator()(SDL_Renderer* p) {
+            if (p != nullptr) {
+                SDL_DestroyRenderer(p);
+            }
+        }
+    };        
+
+    using unique_texture = std::unique_ptr<SDL_Texture, texture_delete>;
+    using unique_surface = std::unique_ptr<SDL_Surface, surface_delete>;
+    using unique_window = std::unique_ptr<SDL_Window, window_delete>;
+    using unique_renderer = std::unique_ptr<SDL_Renderer, renderer_delete>;
+
+    namespace ttf {
+        struct font_delete {
+            void operator()(TTF_Font* p) {
+                TTF_CloseFont(p);
+            }
+        };
+
+        using unique_font = std::unique_ptr<TTF_Font, font_delete>;
+    }
+}
