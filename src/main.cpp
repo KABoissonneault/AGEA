94886e755166385756e5a17d1e51d918ad12b8fb
Kévin Alexandre Boissonneault <kaboissonneault@gmail.com>
2017-12-14 21:32:54 -0500

    Updated main

--- src/main.cpp
+++ src/main.cpp
@@ -5,42 +5,118 @@
 #include <thread>
 #include <string_view>
 #include <algorithm>
+#include <optional>
+#include <map>
+#include <atomic>
 
 #include <expected.hpp>
 #include <gsl/span>
 
 #include "physics/body.h"
 #include "input/event.h"
-#include "sdl.h"
+#include "meta/detected.h"
+#include "view/sdl/sdl.h"
+#include "model/entity.h"
 
 namespace hz {
     namespace {
-        using seconds = std::chrono::duration<double>;
-        using milliseconds = std::chrono::duration<double, std::milli>;
+        template<typename T>
+        using view = gsl::span<T>;
 
-        struct model_entity_t {
-            physics::body body;
+        template<typename Container>
+        auto make_view(Container && c) noexcept -> view<std::remove_reference_t<decltype(*std::forward<Container>(c).data())>> {
+            return gsl::make_span(std::forward<Container>(c));
+        }
+
+        using seconds = std::chrono::duration<double>;
+        using milliseconds = std::chrono::duration<double, std::milli>;        
+
+        class render_resources {
+        public:
+            auto store_font(sdl::ttf::unique_font font, std::string_view font_handle) -> TTF_Font * {
+                auto const[inserted_element, inserted] = fonts.insert_or_assign(std::string(font_handle), std::move(font));
+                (void)inserted;
+                
+                enum { handle, resource };
+                return std::get<resource>(*inserted_element).get();
+            }
+
+            auto get_font(std::string_view font_name) const -> TTF_Font* {
+                enum{ handle, resource };
+                auto const it_found = fonts.find(font_name);
+                if(it_found == fonts.end()) { return nullptr; }
+                return std::get<resource>(*it_found).get();
+            }
+
+        private:
+            std::map<std::string, sdl::ttf::unique_font, std::less<void>> fonts;
         };
 
-        struct view_entity_t {
+        struct body_data {
+            std::atomic<physics::body2d> value;
+        };
+
+        class view_entity_t {
+        public:
             sdl::unique_texture texture;
-            model_entity_t const* entity;
+            std::weak_ptr<body_data const> body;
         };
 
         struct game_model {
-            std::vector<model_entity_t> model_entities;
+            std::vector<model::entity> model_entities;
+            std::vector<std::shared_ptr<body_data>> model_body_data;
             std::vector<view_entity_t> view_entities;
+            render_resources resources;
         };
 
+        class player_input {
+        public:
+            void on_update(model::entity & entity, input::event_state_t const& input) {
+                auto const check_input = [input] (auto const event_up, auto const event_down, auto & flag) {
+                    if(input.has(event_up)) {
+                        flag = true;
+                    } else if(input.has(event_down)) {
+                        flag = false;
+                    }
+                };
+                check_input(input::event_label::up_pressed, input::event_label::up_released, up_pressed);
+                check_input(input::event_label::down_pressed, input::event_label::down_released, down_pressed);
+                check_input(input::event_label::left_pressed, input::event_label::left_released, left_pressed);
+                check_input(input::event_label::right_pressed, input::event_label::right_released, right_pressed);
+
+                auto force = physics::force2d();
+                if(up_pressed) force += physics::force2d(0, input_force);
+                if(down_pressed) force += physics::force2d(0, -input_force);
+                if(left_pressed) force += physics::force2d(-input_force, 0);
+                if(right_pressed) force += physics::force2d(input_force, 0);
+                entity.body = physics::add_force(entity.body, force);
+            }
+
+            double input_force = 1.0;
+
+        private:
+            bool up_pressed = false;
+            bool down_pressed = false;
+            bool left_pressed = false;
+            bool right_pressed = false;
+        };
+
+        class gravity_component {
+        public:
+            void on_update(model::entity & entity) {
+                entity.body = physics::add_force(entity.body, physics::force2d(0, -entity.body.weight.value * 10.0));
+            }
+        };
+        
         auto load_texture(SDL_Renderer & renderer, const char* file_path) -> tl::expected<sdl::unique_texture, int> {
             auto const surface = sdl::unique_surface(SDL_LoadBMP(file_path));
-            if (!surface) {
+            if(!surface) {
                 SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create surface from image: %s", SDL_GetError());
                 return tl::make_unexpected(-1);
             }
 
             auto texture = sdl::unique_texture(SDL_CreateTextureFromSurface(&renderer, surface.get()));
-            if (!texture) {
+            if(!texture) {
                 SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create texture from surface: %s", SDL_GetError());
                 return tl::make_unexpected(-1);
             }
@@ -49,107 +125,143 @@ namespace hz {
         }
 
         auto init_entities(SDL_Renderer & renderer) -> tl::expected<game_model, int> {
-            auto model_entities = std::vector<model_entity_t>(1);
-            model_entities[0] = { physics::body{ physics::position2d{0.0,0.0}, physics::velocity2d{1.0,0.0} } };
+            auto model_entities = std::vector<model::entity>(1);
+            model_entities[0].components.push_back(gravity_component());
+
+            auto entity_data = std::make_shared<body_data>();
 
             auto texture = load_texture(renderer, "assets/white.bmp");
-            if (!texture) {
+            if(!texture) {
                 return tl::make_unexpected(texture.error());
             }
             auto view_entities = std::vector<view_entity_t>(1);
-            view_entities[0] = { std::move(texture).value(), &model_entities[0] };
+            view_entities[0] = {std::move(texture).value(), entity_data};
 
-            return game_model{ std::move(model_entities), std::move(view_entities) };
+            return game_model{std::move(model_entities), {entity_data}, std::move(view_entities)};
         }
 
-        void update_entities(gsl::span<model_entity_t> model_entities, physics::seconds dt) {
-            for (auto & entity : model_entities) {
+        void update_entities(view<model::entity> model_entities, view<std::shared_ptr<body_data>> body_data, input::event_state_t const& input, physics::seconds dt) {
+            for(auto & entity : model_entities) {
+                for(auto & component : entity.components) {
+                    if(component.has_update()) {
+                        component.on_update(entity, input, dt);
+                    }
+                }
                 entity.body = physics::integrate(entity.body, dt);
+                entity.body.acceleration = physics::acceleration2d();
             }
+
+            for(ptrdiff_t i = 0; i < model_entities.size(); ++i) {
+                body_data[i]->value.store(model_entities[i].body);
+            }
+        }
+
+        auto constexpr window_x = 320;
+        auto constexpr window_y = 240;
+        auto constexpr camera_world_x = 100.0;
+        auto constexpr camera_world_y = 75.0;
+
+        auto integer_floor(double d) -> int {
+            return static_cast<int>(d);
         }
 
         void render_entities(gsl::span<view_entity_t> view_entities, SDL_Renderer & renderer) {
             SDL_SetRenderDrawColor(&renderer, 0x00, 0x00, 0x00, 0x00);
-            SDL_RenderClear(&renderer);
+            SDL_RenderClear(&renderer);           
 
-            auto const center_x = 320 / 2;
-            auto const center_y = 240 / 2;
+            for(auto const& entity : view_entities) {
+                auto const body_data = entity.body.lock();
+                if(!body_data) { continue; }
+                auto const body = body_data->value.load();
 
-            for (auto const& entity : view_entities) {
-                auto const dest_target_x = center_x + static_cast<int>((entity.entity->body.position.value.x) / 10.0 * 320);
-                auto const dest_target_y = center_y + static_cast<int>((entity.entity->body.position.value.y) / 7.5 * 240);
-                auto const dest_target = SDL_Rect{ dest_target_x - 32 / 2, dest_target_y - 32 / 2, 32, 32 };
+                auto const center_x = window_x / 2;
+                auto const center_y = window_y / 2;
+                auto const dest_target_x = center_x + integer_floor(body.position.value.x / camera_world_x * window_x);
+                auto const dest_target_y = center_y + integer_floor(-body.position.value.y / camera_world_y * window_y);
+
+                auto const render_width = integer_floor(body.dimension.x / camera_world_x * window_x);
+                auto const render_height = integer_floor(body.dimension.y / camera_world_y * window_y);
+
+                auto const dest_target = SDL_Rect{
+                    dest_target_x - render_width / 2,
+                    dest_target_y - render_height / 2,
+                    render_width,
+                    render_height,
+                };
                 SDL_RenderCopy(&renderer, entity.texture.get(), nullptr, &dest_target);
             }
             SDL_RenderPresent(&renderer);
         }
 
-        using namespace input;
-
-        struct event_state_t {
-        public:
-            event_state_t() = default;
-            explicit event_state_t(std::initializer_list<event_t> events)
-                : events(events) {
-
-            }
-
-            auto has(event_label e) const noexcept -> bool {
-                return std::find(events.begin(), events.end(), event_t(e)) != events.end();
-            }
-            auto has(std::string_view s) const noexcept -> bool {
-                return std::find(events.begin(), events.end(), event_t(s)) != events.end();
-            }
-
-        private:
-            std::vector<event_t> events;
-        };
-
-        auto get_events() -> event_state_t {
-            auto event = SDL_Event();
-            while (SDL_PollEvent(&event)) {
-                if (event.type == SDL_QUIT || event.type == SDL_KEYUP && event.key.keysym.sym == SDLK_ESCAPE) {
-                    return event_state_t{ event_label::exit };
+        auto get_player_input(SDL_Event& e) -> std::optional<input::event_t> {
+            if(e.type == SDL_KEYDOWN) {
+                switch(e.key.keysym.sym) {
+                    case SDLK_UP: return input::event_label::up_pressed;
+                    case SDLK_DOWN: return input::event_label::down_pressed;
+                    case SDLK_LEFT: return input::event_label::left_pressed;
+                    case SDLK_RIGHT: return input::event_label::right_pressed;
+                }
+            } else if(e.type == SDL_KEYUP) {
+                switch(e.key.keysym.sym) {
+                    case SDLK_UP: return input::event_label::up_released;
+                    case SDLK_DOWN: return input::event_label::down_released;
+                    case SDLK_LEFT: return input::event_label::left_released;
+                    case SDLK_RIGHT: return input::event_label::right_released;
                 }
             }
 
-            return {};
+            return std::nullopt;
         }
 
-        auto game_loop(SDL_Renderer& renderer) -> tl::expected<tl::monostate, int> {
-            auto result = init_entities(renderer);
-            if (!result) {
-                return tl::make_unexpected(result.error());
-            }
-            auto game_model = std::move(result).value();
+        auto get_events() -> input::event_state_t {
+            auto event_state = input::event_state_t();
 
+            auto event = SDL_Event();
+            while(SDL_PollEvent(&event)) {
+                if(event.type == SDL_QUIT || event.type == SDL_KEYUP && event.key.keysym.sym == SDLK_ESCAPE) {
+                    event_state.push(input::event_label::exit);
+                }
+
+                if(auto const player_input = get_player_input(event)) {
+                    event_state.push(*player_input);
+                }
+            }
+
+            return event_state;
+        }
+
+        void do_game_loop(SDL_Renderer& renderer, game_model & model) {
             auto constexpr frame_duration = milliseconds(1.0 / 60.0);
             auto frame_buffer = milliseconds();
-            while (true) {
+            while(true) {
                 auto const frame_start = std::chrono::steady_clock::now();
 
                 auto const event_state = get_events();
-                if (event_state.has(event_label::exit)) {
+                if(event_state.has(input::event_label::exit)) {
                     break;
                 }
 
-                while (frame_buffer > frame_duration) {
-                    update_entities(game_model.model_entities, frame_duration);
+                while(frame_buffer > frame_duration) {
+                    update_entities(model.model_entities, model.model_body_data, event_state, frame_duration);
                     frame_buffer -= frame_duration;
                 }
 
-                render_entities(game_model.view_entities, renderer);
+                render_entities(model.view_entities, renderer);
 
                 auto const frame_complete = std::chrono::steady_clock::now();
                 auto const frame_completion_duration = frame_complete - frame_start;
-                if (frame_completion_duration < frame_duration) {
+                if(frame_completion_duration < frame_duration) {
                     std::this_thread::sleep_for(frame_duration - frame_completion_duration);
                 }
                 auto const frame_end = std::chrono::steady_clock::now();
                 frame_buffer += frame_end - frame_start;
             }
 
-            return {};
+        }
+
+        auto game_loop(SDL_Renderer& renderer) -> tl::expected<tl::monostate, int> {
+            auto game_result = init_entities(renderer);
+            return game_result.map([&renderer] (game_model& model) { do_game_loop(renderer, model); });
         }
     }
 }
@@ -158,18 +270,23 @@ namespace {
     namespace sdl = hz::sdl;
 
     auto sdl_init() -> int {
-        auto const error = SDL_Init(0);
-        if (error < 0) {
+        if(auto const error = SDL_Init(0); error < 0) {
             SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Unable to initialize SDL: %s", SDL_GetError());
             return error;
         }
         std::atexit(SDL_Quit);
+
+        if(auto const error = TTF_Init(); error < 0) {
+            SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Unable to initialize TTF: %s", SDL_GetError());
+            return error;
+        }
+        std::atexit(TTF_Quit);
         return {};
     }
 
     auto sdl_sub_init() -> int {
         auto const error = SDL_InitSubSystem(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
-        if (error < 0) {
+        if(error < 0) {
             SDL_Log("Unable to initialize SDL_video or SDL_events: %s", SDL_GetError());
             return error;
         }
@@ -180,8 +297,8 @@ namespace {
     auto make_window_and_renderer() -> tl::expected<std::pair<sdl::unique_window, sdl::unique_renderer>, int> {
         SDL_Window* window;
         SDL_Renderer* renderer;
-        if (auto const error = SDL_CreateWindowAndRenderer(320, 240, SDL_WINDOW_RESIZABLE, &window, &renderer)
-            ; error < 0) {
+        if(auto const error = SDL_CreateWindowAndRenderer(hz::window_x, hz::window_y, SDL_WINDOW_RESIZABLE, &window, &renderer)
+           ; error < 0) {
             SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create surface from image: %s", SDL_GetError());
             return tl::make_unexpected(error);
         }
@@ -192,21 +309,22 @@ namespace {
 
 
 auto main(int argc, char* argv[]) -> int {
-	if (auto const error = sdl_init(); error < 0) {
-		return error;
-	}
+    (void)argc, (void)argv;
+    if(auto const error = sdl_init(); error < 0) {
+        return error;
+    }
 
-	if (auto const error = sdl_sub_init(); error < 0) {
-		return error;
-	}
-	
-	auto result = make_window_and_renderer();
-	if (!result) {
-		return result.error();
-	}
-	auto const [window, renderer] = std::move(result).value();
-	(void)window;	
+    if(auto const error = sdl_sub_init(); error < 0) {
+        return error;
+    }
 
-	auto const game_result = hz::game_loop(*renderer.get());
-	return game_result ? 0 : game_result.error();
+    auto result = make_window_and_renderer();
+    if(!result) {
+        return result.error();
+    }
+    auto const[window, renderer] = std::move(result).value();
+    (void)window;
+
+    auto const game_result = hz::game_loop(*renderer.get());
+    return game_result ? 0 : game_result.error();
 }
